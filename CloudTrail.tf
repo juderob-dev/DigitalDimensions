730b01713d5e2e4737c4286833bb3e48c12bd981
juderob-dev <juderoberts09@gmail.com>
2022-05-26 00:40:36 +0100

    added templetae for sqs messaging, use emil at endpoint

--- CloudTrail.tf
+++ CloudTrail.tf
@@ -183,3 +183,8 @@ data "aws_iam_policy_document" "sns_topic_policy" {
     sid = "__default_statement_ID"
   }
 }
+resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
+  topic_arn = "arn:aws:sns:us-west-2:432981146916:user-updates-topic"
+  protocol  = "sqs"
+  endpoint  = "arn:aws:sqs:us-west-2:432981146916:terraform-queue-too"
+}
